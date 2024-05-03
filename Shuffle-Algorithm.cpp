1adf15f9ea982ec4d1a5926ba5558bbb1818238e
TheRealMilesLee <grandtheftauto710@outlook.com>
2024-05-02 23:15:14 -0700

    Add the DP for shuffle problem

--- Shuffle-Algorithm.cpp
+++ Shuffle-Algorithm.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 bool shuffle_BruteForce(string s, string PatternX, string PatternY);
 
+bool shuffle_DynamicProgramming(string s, string PatternX, string PatternY);
 int main()
 {
   string s = "caaabaacb";
@@ -22,6 +23,25 @@ int main()
 
   string s2 = "ccaaaabab";
   if (shuffle_BruteForce(s2, PatternX, PatternY))
+  {
+    cout << "Yes, " << s2 << " are shuffle of " << PatternX << " and " << PatternY << endl;
+  }
+  else
+  {
+    cout << "No, " << s2 << " are not shuffle of " << PatternX << " and " << PatternY << endl;
+  }
+
+  cout << "Now using DP" << endl;
+  if (shuffle_DynamicProgramming(s, PatternX, PatternY))
+  {
+    cout << "Yes, " << s << " are shuffle of " << PatternX << " and " << PatternY << endl;
+  }
+  else
+  {
+    cout << "No, " << s << " are not shuffle of " << PatternX << " and " << PatternY << endl;
+  }
+
+  if (shuffle_DynamicProgramming(s2, PatternX, PatternY))
   {
     cout << "Yes, " << s2 << " are shuffle of " << PatternX << " and " << PatternY << endl;
   }
@@ -59,10 +79,31 @@ bool shuffle_BruteForce(string s, string PatternX, string PatternY)
   {
     return false;
   }
-  if (X != PatternX && Y != PatternY)
+  else if (X != PatternX || Y != PatternY)
   {
     cout << "X: " << X << " Y: " << Y << endl;
     return false;
   }
   return true;
+}
+
+bool shuffle_DynamicProgramming(string s, string PatternX, string PatternY)
+{
+  vector<vector<bool>> dp(PatternX.size() + 1, vector<bool>(PatternY.size() + 1, false));
+  dp[0][0] = true;
+  for (size_t i = 0; i <= PatternX.size(); i++)
+  {
+    for (size_t j = 0; j <= PatternY.size(); j++)
+    {
+      if (i > 0 && s[i + j - 1] == PatternX[i - 1])
+      {
+        dp[i][j] = dp[i][j] || dp[i - 1][j];
+      }
+      if (j > 0 && s[i + j - 1] == PatternY[j - 1])
+      {
+        dp[i][j] = dp[i][j] || dp[i][j - 1];
+      }
+    }
+  }
+  return dp[PatternX.size()][PatternY.size()];
 }
